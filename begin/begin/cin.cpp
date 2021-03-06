3725ba69ca96834bf8c78c32413954e0a115a9fd
VincentHyf <1004779785@qq.com>
2021-03-06 13:23:58 +0800

    add: cin

---
+++ begin/begin/cin.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int floatNumber = 0;
+	cout << "Please input a Number" << endl;
+	cin >> floatNumber;
+	cout << "Varius = " << floatNumber << endl;
+	return 0;
+}
